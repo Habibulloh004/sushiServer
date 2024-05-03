ef1e051901b057a845efa0a76efbe68ce9b70508
habibulloh004 <habibulloh4080@gmail.com>
2024-05-03 16:01:50 +0500

    nmadr

--- index.js
+++ index.js
@@ -146,12 +146,14 @@ app.get("/getOrders/:id", async (req, res) => {
 });
 
 app.get("/getTransaction", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://kuryer-sushi.vercel.app/");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  // Optionally, you can set more headers as needed
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://kuryer-sushi.vercel.app/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST"); // Adjust methods if needed
+  // Optionally, set more headers as needed
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials if needed
 
-  // Allow credentials if needed
-  res.setHeader("Access-Control-Allow-Credentials", "true");
   const { id, date } = req.query;
   const response = await axios.get(
     `https://joinposter.com/api/dash.getTransactions?token=${
