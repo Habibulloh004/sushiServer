5d8fc062861b3b190af31dae10a46c025ee33f22
OybekDeveloper <Programmeroybek211@gmail.com>
2025-05-10 22:09:44 +0500

    send request to poster and abdugani

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -3,11 +3,15 @@ import transactionModel from "../models/transaction.model.js";
 class TransactionController {
   async getById(req, res, next) {
     try {
-      const { id } = req.param;
+      const { id } = req.params;
       const transaction = await transactionModel.findOne({ _id: id });
+      if (!transaction) {
+        return res.status(404).send("Transaction not found");
+      }
       res.send(transaction);
     } catch (error) {
-      res.send(`not found transaction ${error}`)
+      console.error("Error fetching transaction:", error);
+      res.status(500).send(`Error: ${error.message}`);
     }
   }
 }
